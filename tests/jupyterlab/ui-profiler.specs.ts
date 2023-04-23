5df69b7ed242efdefcf10185b9160fdfb4fe4373
krassowski <5832902+krassowski@users.noreply.github.com>
2023-04-23 20:22:58 +0100

    Rewrite using new version of ui-profiler

--- tests/jupyterlab/ui-profiler.specs.ts
+++ tests/jupyterlab/ui-profiler.specs.ts
@@ -1,22 +1,21 @@
 import { benchmark, galata, test } from '@jupyterlab/galata';
 import type { ContentsHelper } from '@jupyterlab/galata/lib/contents';
+import type { IUIProfiler, IBenchmarkResult, ITimingOutcome } from '@jupyterlab/ui-profiler'
+import type { MenuOpenScenarioOptions } from '@jupyterlab/ui-profiler/lib/types/_scenario-menu-open';
+import type { ExecutionTimeBenchmarkOptions } from '@jupyterlab/ui-profiler/lib/types/_benchmark-execution';
 import * as path from 'path';
-
-const PROFILER_CARD_SELECTOR =
-  '.jp-LauncherCard[title="Open JupyterLab UI Profiler"]';
-
-
-const START_BUTTON_SELECTOR =
-  '.up-BenchmarkLauncher-launchbar-buttons .jp-mod-accept';
-
-const REPEATS_INPUT_SELECTOR = '#up-profiler-benchmark_repeats';
-
-const COMPLETED_SELECTOR = '.up-mod-completed';
 
 const fileNames = [
   'gh-9757-reproducer.ipynb',
   'all-html-elements.ipynb'
 ];
+
+const PROFILER_PLUGIN = '@jupyterlab/ui-profiler:plugin';
+
+
+function getProfiler(): IUIProfiler {
+  return (window as any).jupyterapp._plugins.get(PROFILER_PLUGIN).service;
+}
 
 
 test.describe('Benchmark using UI Profiler', () => {
@@ -45,31 +44,27 @@ test.describe('Benchmark using UI Profiler', () => {
     };
   });
 
-  test.beforeEach(async ({page}) => {
-    const card = await page.waitForSelector(PROFILER_CARD_SELECTOR);
-    await card.click();
-  });
-
   test('open menu', async ({ page, tmpPath }, testInfo) => {
 
     // TODO: open a heavy notebook `all-html-elements.ipynb` in background
     // this should be implemented in jupyterlab-ui-profiler as a new field.
 
-    const startButton = await page.waitForSelector(START_BUTTON_SELECTOR);
-    await startButton.click();
-
-    await page.waitForSelector(COMPLETED_SELECTOR);
-
-    const repeatsInput = await page.waitForSelector(REPEATS_INPUT_SELECTOR);
-    await repeatsInput.fill(benchmark.nSamples.toString());
-
-    const resultHandle = await page.waitForSelector(
-      '.up-BenchmarkHistory-file >> text=execution-time_menuOpen'
+    const result: IBenchmarkResult<ITimingOutcome> = await page.evaluate(
+      async ([repeats]) => {
+        const profiler = getProfiler();
+        const result = await profiler.runBenchmark({
+          id: 'menuOpen',
+          options: {"menu": "file"} as MenuOpenScenarioOptions
+        }, {
+          id: 'execution-time',
+          options: {"repeats": repeats} as ExecutionTimeBenchmarkOptions
+        });
+        return result;
+      },
+      [benchmark.nSamples]
     );
-    const resultFileName = await resultHandle.innerText();
-    const file = await contents.getContentMetadata(`${tmpPath}/ui-profiler-results/${resultFileName}`);
 
-    const times = JSON.parse(file.content).result.reference;
+    const times = result.outcome.reference;
     for (let time of times) {
       testInfo.attachments.push(
         benchmark.addAttachment({
